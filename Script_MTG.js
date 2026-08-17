ffafd63ee187c57ecafe53fc55dafc5308db34e1
Loic <>
2026-08-17 20:36:16 +0200

    no message

--- Script_MTG.js
+++ Script_MTG.js
@@ -202,7 +202,7 @@ function modifyJsonFile(inputFilePath, outputFilePath, allCardsPath) {
         }
     });
 
-   rl.on('close', () => {
+    rl.on('close', () => {
         const result = {};
         const rlOracle = readline.createInterface({
             input: fs.createReadStream(inputFilePath),
@@ -276,6 +276,8 @@ function modifyJsonFile(inputFilePath, outputFilePath, allCardsPath) {
             }
         });
 
+        addTreacheryCards(result)
+
         rlOracle.on('close', () => {
             fs.writeFile(outputFilePath, JSON.stringify(result, null, 2), 'utf8', (err) => {
                 if (err) console.error('Erreur écriture output JSON:', err);
@@ -320,3 +322,484 @@ function increaseGameVersion() {
 // Utilisation
 //modifyJsonFile('oracle.json', 'MTGCards.json', 'all.json');
 module.exports = modifyJsonFile;
+
+
+
+
+
+
+function addTreacheryCards(cardList) {
+    for (const [subtype, cards] of Object.entries(treacheryData)) {
+
+        for (const card of cards) {
+            const cardId = "treachery-" + card.id
+            cardList[cardId] = {
+                id: cardId,
+                name: card.name,
+                type: "Emblem",
+                isToken: true,
+                cost: card.cmc,
+                face: {
+                    front: {
+                        name: card.name,
+                        image: card.image,
+                        type: "Emblem"
+                    }
+                },
+                Colors: [],
+                "Card type": "Emblem",
+                "Color identity": [],
+                set: "Treachery",
+                isHorizontal: false
+            }
+        }
+    }
+}
+
+
+
+
+const treacheryData = {
+    "Guardian": [
+        {
+            "id": 1,
+            "name": "The Ætherist",
+            "cmc": 0,
+            "rarity": "R",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/001%20-%20Guardian%20-%20The%20Ætherist.jpg"
+        },
+        {
+            "id": 2,
+            "name": "The Augur",
+            "cmc": 0,
+            "rarity": "R",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/002%20-%20Guardian%20-%20The%20Augur.jpg"
+        },
+        {
+            "id": 3,
+            "name": "The Bodyguard",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/003%20-%20Guardian%20-%20The%20Bodyguard.jpg"
+        },
+        {
+            "id": 4,
+            "name": "The Cathar",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/004%20-%20Guardian%20-%20The%20Cathar.jpg"
+        },
+        {
+            "id": 5,
+            "name": "The Cryomancer",
+            "cmc": 0,
+            "rarity": "R",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/005%20-%20Guardian%20-%20The%20Cryomancer.jpg"
+        },
+        {
+            "id": 6,
+            "name": "The Flickering Mage",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/006%20-%20Guardian%20-%20The%20Flickering%20Mage.jpg"
+        },
+        {
+            "id": 7,
+            "name": "The Golem",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/007%20-%20Guardian%20-%20The%20Golem.jpg"
+        },
+        {
+            "id": 8,
+            "name": "The Great Martyr",
+            "cmc": 0,
+            "rarity": "M",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/008%20-%20Guardian%20-%20The%20Great%20Martyr.jpg"
+        },
+        {
+            "id": 9,
+            "name": "The Immortal",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/009%20-%20Guardian%20-%20The%20Immortal.jpg"
+        },
+        {
+            "id": 10,
+            "name": "The Inquisitor",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/010%20-%20Guardian%20-%20The%20Inquisitor.jpg"
+        },
+        {
+            "id": 11,
+            "name": "The Marshal",
+            "cmc": 0,
+            "rarity": "R",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/011%20-%20Guardian%20-%20The%20Marshal.jpg"
+        },
+        {
+            "id": 12,
+            "name": "The Mirror Maestra",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/012%20-%20Guardian%20-%20The%20Mirror%20Maestra.jpg"
+        },
+        {
+            "id": 13,
+            "name": "The Oracle",
+            "cmc": 0,
+            "rarity": "M",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/013%20-%20Guardian%20-%20The%20Oracle.jpg"
+        },
+        {
+            "id": 14,
+            "name": "The Quellmaster",
+            "cmc": 0,
+            "rarity": "R",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/014%20-%20Guardian%20-%20The%20Quellmaster.jpg"
+        },
+        {
+            "id": 15,
+            "name": "The Spellsnatcher",
+            "cmc": 0,
+            "rarity": "R",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/015%20-%20Guardian%20-%20The%20Spellsnatcher.jpg"
+        },
+        {
+            "id": 16,
+            "name": "The Summoner",
+            "cmc": 0,
+            "rarity": "R",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/016%20-%20Guardian%20-%20The%20Summoner.jpg"
+        },
+        {
+            "id": 17,
+            "name": "The Supplier",
+            "cmc": 0,
+            "rarity": "M",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/017%20-%20Guardian%20-%20The%20Supplier.jpg"
+        },
+        {
+            "id": 18,
+            "name": "The Warlock",
+            "cmc": 0,
+            "rarity": "M",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/018%20-%20Guardian%20-%20The%20Warlock.jpg"
+        }
+    ],
+    "Traitor": [
+        {
+            "id": 19,
+            "name": "The Banisher",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/019%20-%20Traitor%20-%20The%20Banisher.jpg"
+        },
+        {
+            "id": 20,
+            "name": "The Cleaner",
+            "cmc": 0,
+            "rarity": "R",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/020%20-%20Traitor%20-%20The%20Cleaner.jpg"
+        },
+        {
+            "id": 21,
+            "name": "The Ferryman",
+            "cmc": 0,
+            "rarity": "M",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/021%20-%20Traitor%20-%20The%20Ferryman.jpg"
+        },
+        {
+            "id": 22,
+            "name": "The Gatekeeper",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/022%20-%20Traitor%20-%20The%20Gatekeeper.jpg"
+        },
+        {
+            "id": 23,
+            "name": "The Grenadier",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/023%20-%20Traitor%20-%20The%20Grenadier.jpg"
+        },
+        {
+            "id": 24,
+            "name": "He Who Comes To Save The Day",
+            "cmc": 0,
+            "rarity": "M",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/024%20-%20Traitor%20-%20He%20Who%20Comes%20To%20Save%20The%20Day.jpg"
+        },
+        {
+            "id": 25,
+            "name": "The Metamorph",
+            "cmc": 0,
+            "rarity": "S",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/025%20-%20Traitor%20-%20The%20Metamorph.jpg"
+        },
+        {
+            "id": 26,
+            "name": "The Oneiromancer",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/026%20-%20Traitor%20-%20The%20Oneiromancer.jpg"
+        },
+        {
+            "id": 27,
+            "name": "The Puppet Master",
+            "cmc": 0,
+            "rarity": "S",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/027%20-%20Traitor%20-%20The%20Puppet%20Master.jpg"
+        },
+        {
+            "id": 28,
+            "name": "The Reflector",
+            "cmc": 0,
+            "rarity": "R",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/028%20-%20Traitor%20-%20The%20Reflector.jpg"
+        },
+        {
+            "id": 29,
+            "name": "The Time Bender",
+            "cmc": 0,
+            "rarity": "R",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/029%20-%20Traitor%20-%20The%20Time%20Bender.jpg"
+        },
+        {
+            "id": 30,
+            "name": "The Treacherous Masochist",
+            "cmc": 0,
+            "rarity": "S",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/030%20-%20Traitor%20-%20The%20Treacherous%20Masochist.jpg"
+        },
+        {
+            "id": 31,
+            "name": "The Wearer of Masks",
+            "cmc": 0,
+            "rarity": "S",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/031%20-%20Traitor%20-%20The%20Wearer%20of%20Masks.jpg"
+        }
+    ],
+    "Assassin": [
+        {
+            "id": 32,
+            "name": "The Ambitious Queen",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/032%20-%20Assassin%20-%20The%20Ambitious%20Queen.jpg"
+        },
+        {
+            "id": 33,
+            "name": "The Beastmaster",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/033%20-%20Assassin%20-%20The%20Beastmaster.jpg"
+        },
+        {
+            "id": 34,
+            "name": "The Bio-Engineer",
+            "cmc": 0,
+            "rarity": "M",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/034%20-%20Assassin%20-%20The%20Bio-Engineer.jpg"
+        },
+        {
+            "id": 35,
+            "name": "The Chaos Wielder",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/035%20-%20Assassin%20-%20The%20Chaos%20Wielder.jpg"
+        },
+        {
+            "id": 36,
+            "name": "The Corpse Snatcher",
+            "cmc": 0,
+            "rarity": "R",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/036%20-%20Assassin%20-%20The%20Corpse%20Snatcher.jpg"
+        },
+        {
+            "id": 37,
+            "name": "The Demon",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/037%20-%20Assassin%20-%20The%20Demon.jpg"
+        },
+        {
+            "id": 38,
+            "name": "The Depths Caller",
+            "cmc": 0,
+            "rarity": "M",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/038%20-%20Assassin%20-%20The%20Depths%20Caller.jpg"
+        },
+        {
+            "id": 39,
+            "name": "The Madwoman",
+            "cmc": 0,
+            "rarity": "R",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/039%20-%20Assassin%20-%20The%20Madwoman.jpg"
+        },
+        {
+            "id": 40,
+            "name": "The Necromancer",
+            "cmc": 0,
+            "rarity": "R",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/040%20-%20Assassin%20-%20The%20Necromancer.jpg"
+        },
+        {
+            "id": 41,
+            "name": "The Physician",
+            "cmc": 0,
+            "rarity": "R",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/041%20-%20Assassin%20-%20The%20Physician.jpg"
+        },
+        {
+            "id": 42,
+            "name": "The Pyromancer",
+            "cmc": 0,
+            "rarity": "R",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/042%20-%20Assassin%20-%20The%20Pyromancer.jpg"
+        },
+        {
+            "id": 43,
+            "name": "The Rebel General",
+            "cmc": 0,
+            "rarity": "R",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/043%20-%20Assassin%20-%20The%20Rebel%20General.jpg"
+        },
+        {
+            "id": 44,
+            "name": "The Seer",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/044%20-%20Assassin%20-%20The%20Seer.jpg"
+        },
+        {
+            "id": 45,
+            "name": "The Shapeshifting Slayer",
+            "cmc": 0,
+            "rarity": "M",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/045%20-%20Assassin%20-%20The%20Shapeshifting%20Slayer.jpg"
+        },
+        {
+            "id": 46,
+            "name": "The Sigil Mage",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/046%20-%20Assassin%20-%20The%20Sigil%20Mage.jpg"
+        },
+        {
+            "id": 47,
+            "name": "The Sorceress",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/047%20-%20Assassin%20-%20The%20Sorceress.jpg"
+        },
+        {
+            "id": 48,
+            "name": "The Villain",
+            "cmc": 0,
+            "rarity": "R",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/048%20-%20Assassin%20-%20The%20Villain.jpg"
+        },
+        {
+            "id": 49,
+            "name": "The War Shaman",
+            "cmc": 0,
+            "rarity": "R",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/049%20-%20Assassin%20-%20The%20War%20Shaman.jpg"
+        }
+    ],
+    "Leader": [
+        {
+            "id": 50,
+            "name": "The Blood Empress",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/050%20-%20Leader%20-%20The%20Blood%20Empress.jpg"
+        },
+        {
+            "id": 51,
+            "name": "The Chaos Bringer",
+            "cmc": 0,
+            "rarity": "M",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/051%20-%20Leader%20-%20The%20Chaos%20Bringer.jpg"
+        },
+        {
+            "id": 52,
+            "name": "The Corrupted Regent",
+            "cmc": 0,
+            "rarity": "R",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/052%20-%20Leader%20-%20The%20Corrupted%20Regent.jpg"
+        },
+        {
+            "id": 53,
+            "name": "The Debt Collector",
+            "cmc": 0,
+            "rarity": "M",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/053%20-%20Leader%20-%20The%20Debt%20Collector.jpg"
+        },
+        {
+            "id": 54,
+            "name": "The Gathering",
+            "cmc": 0,
+            "rarity": "M",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/054%20-%20Leader%20-%20The%20Gathering.jpg"
+        },
+        {
+            "id": 55,
+            "name": "Her Seedborn Highness",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/055%20-%20Leader%20-%20Her%20Seedborn%20Highness.jpg"
+        },
+        {
+            "id": 56,
+            "name": "His Beloved Majesty",
+            "cmc": 0,
+            "rarity": "M",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/056%20-%20Leader%20-%20His%20Beloved%20Majesty.jpg"
+        },
+        {
+            "id": 57,
+            "name": "The King over the Scrapyard",
+            "cmc": 0,
+            "rarity": "R",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/057%20-%20Leader%20-%20The%20King%20over%20the%20Scrapyard.jpg"
+        },
+        {
+            "id": 58,
+            "name": "The Lich Queen",
+            "cmc": 0,
+            "rarity": "M",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/058%20-%20Leader%20-%20The%20Lich%20Queen.jpg"
+        },
+        {
+            "id": 59,
+            "name": "The Old Ruler",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/059%20-%20Leader%20-%20The%20Old%20Ruler.jpg"
+        },
+        {
+            "id": 60,
+            "name": "The Queen of Light",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/060%20-%20Leader%20-%20The%20Queen%20of%20Light.jpg"
+        },
+        {
+            "id": 61,
+            "name": "The Twin Princesses",
+            "cmc": 0,
+            "rarity": "R",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/061%20-%20Leader%20-%20The%20Twin%20Princesses.jpg"
+        },
+        {
+            "id": 62,
+            "name": "The Void Tyrant",
+            "cmc": 0,
+            "rarity": "U",
+            "image": "https://mtgtreachery.net/images/cards/en/trd/062%20-%20Leader%20-%20The%20Void%20Tyrant.jpg"
+        }
+    ]
+}
